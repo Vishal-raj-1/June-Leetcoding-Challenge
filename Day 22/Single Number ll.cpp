9f42713fefb8ecd1b7c1514cbc62e810f55e18f2
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-06-24 17:04:18 +0530

    done

---
+++ Day 22/Single Number ll.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        int bits[32] = {0};
+        int result = 0;
+
+        for(int i=0; i<32; i++)
+        {
+            for(int n : nums)
+            {
+                bits[i] += (n >> i)&1;
+                bits[i] %= 3;
+            }
+        }
+
+        for(int i=0; i<32; i++)
+            result |= (bits[i]<<i);
+
+        return result;
+    }
+};
---
+++ Day 22/Single_Number lll.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int>& nums)
+    {
+        //Xor of all element
+        int Xor = nums[0];
+
+        //To divide vector in two part
+        //one have this set bit another one doesn't
+        int rightSetBit;
+        int n = nums.size();
+
+        for(int i=1; i<n; i++)
+            Xor ^= nums[i];
+
+        rightSetBit = Xor & ~(Xor - 1);
+
+        vector<int>ans(2);
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] & rightSetBit)
+                ans[0] ^= nums[i];
+            else
+                ans[1] ^= nums[i];
+        }
+
+        return ans;
+    }
+};
