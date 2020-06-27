39057123ad1ebf9b9c3665d6bc27a0a970159ad6
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-06-27 23:56:19 +0530

    Create Perfect_Squares.cpp

--- Day 27/Perfect_Squares.cpp
+++ Day 27/Perfect_Squares.cpp
@@ -4,7 +4,6 @@ public:
     int numSquares(int n)
     {
         vector<int>dp(n+1,n);
-        
         dp[0] = 0,dp[1] = 1;
         
         for(int i=2;i<=n;i++)
@@ -12,7 +11,6 @@ public:
             for(int j=1;j*j <= i;j++)
                 dp[i] = min(dp[i],dp[i - j*j] + 1);
         }
-        
         return dp[n];
     }
 };
