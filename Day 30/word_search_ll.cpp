003c9af540a37d63cdf957d14d271ba83d5cdfc0
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-07-01 20:12:54 +0530

    done

---
+++ Day 30/word_search_ll.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    bool check(vector<vector<char>>& board,int i,int j,int row,int col,int index,string word)
+    {
+        if(index == word.size())
+            return true;
+
+        if(i < 0 || j < 0 || i >= row || j >= col || board[i][j] == '#' || board[i][j] != word[index])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        bool found = check(board,i+1,j,row,col,index+1,word) || check(board,i,j+1,row,col,index+1,word) ||
+                     check(board,i-1,j,row,col,index+1,word) || check(board,i,j-1,row,col,index+1,word);
+
+        board[i][j] = temp;
+
+        return found;
+    }
+
+    vector<string> findWords(vector<vector<char>>& board,vector<string>& words)
+    {
+        int row = board.size();
+        int col = board[0].size();
+        int ws = words.size();
+
+        unordered_set<string> ans;
+
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                for(int k=0; k<ws; k++)
+                {
+                    if(board[i][j] == words[k][0] && check(board,i,j,row,col,0,words[k]))
+                      {
+                          ans.insert(words[k]);
+                     
+                      }
+                }
+            }
+        }
+        
+        vector<string>v(ans.begin(),ans.end());
+        
+        return v;
+    }
+};
